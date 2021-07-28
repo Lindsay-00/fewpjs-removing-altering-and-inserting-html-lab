bbce0d94b567919871ad64ac30f20bdc7551f9f8
Lindsay <lindsay.chen@yale.edu>
2021-07-28 09:52:44 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const r = document.getElementById("main")
+r.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Lindsay is the champion!"
+
+
